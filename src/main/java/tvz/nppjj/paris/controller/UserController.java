6805dc0969b908de0a0978e03135af51bb7c728b
Filip <filip.corluka@gmail.com>
2016-05-29 18:10:48 +0200

    User controller fix

--- src/main/java/tvz/nppjj/paris/controller/UserController.java
+++ src/main/java/tvz/nppjj/paris/controller/UserController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import tvz.nppjj.paris.model.User;
 import tvz.nppjj.paris.service.UserService;
 
+@RestController
 public class UserController {
 
 	@Autowired
