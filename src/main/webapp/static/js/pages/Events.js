bef8e395ce4750034bc8f9ebf6fb98ad5a9fae1d
denix00 <denix00@gmail.com>
2016-06-08 03:02:29 +0200

    prikazivanje evenata s API-ja

--- src/main/webapp/static/js/pages/Events.js
+++ src/main/webapp/static/js/pages/Events.js
@@ -7,8 +7,8 @@ export default class Events extends React.Component {
 
   constructor(props){
     super(props);
-      this.response = [];
-  };
+      this.state = {response:[]};
+    }
 
   componentDidMount(){
     $.ajax({
@@ -18,49 +18,17 @@ export default class Events extends React.Component {
       type: 'GET'
     }).done(function (data){
       this.setState({response: data});
+      // console.log(JSON.stringify(this.state.response));
     });
   }
 
   render() {
-    const AllEvents =
-    [
-    {"id":1,
-    "name":"Euro2016",
-    "location":"Stade de France",
-    "city":"Paris",
-    "date":"2016-06-10",
-    "description":"Utakmica Eura 2016.",
-    "price":51.55,
-    "picture":"//"
-    ,"category":{"id":1,"name":"Nogomet"}},
-
-    {"id":2,
-    "name":"Humanitarni koncert",
-    "location":"Slavonska avenija 7",
-    "city":"Zagreb",
-    "date":"2016-06-21",
-    "description":"Koncert u svrhu skupljanja sredstava za siromašne",
-    "price":11.51,
-    "picture":"//",
-    "category":{"id":2,"name":"Glazba"}},
-
-    {"id":3,
-    "name":"Inception",
-    "location":"Branimirova",
-    "city":"Zagreb",
-    "date":"2016-06-10",
-    "description":"Prikazivanje filma s glavnom ulogom Leonarda Di Capria",
-    "price":111.11,
-    "picture":"//",
-    "category":{"id":3,"name":"Filmovi"}}
-  ].map((eventAPI, i) => <Event key={i} event={eventAPI} />)
-
     return (
       <div>
         <FilterForm />
         <hr/>
         <div class="row events-event">
-          {AllEvents}
+          { this.state.response.map((eventAPI, i) => <Event key={i} event={eventAPI} />)  }
         </div>
       </div>
     );
