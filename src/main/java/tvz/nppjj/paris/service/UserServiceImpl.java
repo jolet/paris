f17859ed620a84b81763811749731e6afb410c7b
jkovacek <josipkovacek@yahoo.com>
2016-06-08 23:57:51 -0700

    added controller and service test

--- src/main/java/tvz/nppjj/paris/service/UserServiceImpl.java
+++ src/main/java/tvz/nppjj/paris/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import tvz.nppjj.paris.model.User;
 import tvz.nppjj.paris.model.dto.UserDto;
@@ -13,55 +14,55 @@ import tvz.nppjj.paris.model.mapper.UserMapper;
 import tvz.nppjj.paris.repository.UserRepository;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private RoleService roleService;
+    @Autowired
+    private RoleService roleService;
 
-	@Override
-	public List<User> getAllUsers() {
-		return (List<User>) userRepository.findAll();
-	}
+    @Override
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
 
-	@Override
-	public void saveUser(User user) {
-		userRepository.saveAndFlush(user);
+    @Override
+    public void saveUser(User user) {
+        userRepository.saveAndFlush(user);
 
-	}
+    }
 
-	@Override
-	public User getUserById(Long id) {
-		return userRepository.findOne(id);
-	}
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findOne(id);
+    }
 
-	@Override
-	public void registerUser(User user) {
-		// Check if user already exists, throw exception if it does.
-		// Exception will be propagated back to calling controller and returned
-		// through ajax error response
-		User checkUser = userRepository.findByEmail(user.getEmail());
-		if (checkUser != null) {
-			throw new ParisException("User with email " + user.getEmail() + " already exists!");
-		}
+    @Override
+    public User registerUser(User user) {
+        // Check if user already exists, throw exception if it does.
+        // Exception will be propagated back to calling controller and returned
+        // through ajax error response
+        User checkUser = userRepository.findByEmail(user.getEmail());
+        if (checkUser != null) {
+            throw new ParisException("User with email " + user.getEmail() + " already exists!");
+        }
 
-		// add default user role for now
-		user.setRole(roleService.findByRoleType(RoleType.USER));
-		userRepository.save(user);
+        // add default user role for now
+        user.setRole(roleService.findByRoleType(RoleType.USER));
+        return userRepository.save(user);
 
-		// TODO: send email notification...
-	}
+        // TODO: send email notification...
+    }
 
-	@Override
-	public UserDto loginUser(String username, String password) {
-		User user=userRepository.findByUsernameAndPassword(username, password);
-		if (user == null){
-			throw new ParisException("That user do not exist!");
-		}
-		return UserMapper.transformUserToUserDto(user);
-	}
-	
+    @Override
+    public UserDto loginUser(String username, String password) {
+        User user = userRepository.findByUsernameAndPassword(username, password);
+        if (user == null) {
+            throw new ParisException("That user do not exist!");
+        }
+        return UserMapper.transformUserToUserDto(user);
+    }
 
 }
---
+++ src/test/java/tvz/nppjj/paris/controller/RegistrationControllerTest.java
@@ -0,0 +1,126 @@
+package tvz.nppjj.paris.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import tvz.nppjj.paris.init.WebNppjjParisApplication;
+import tvz.nppjj.paris.model.dto.RegistrationDto;
+import tvz.nppjj.paris.repository.TicketRepository;
+import tvz.nppjj.paris.repository.UserRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WebNppjjParisApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port=0")
+public class RegistrationControllerTest {
+
+    @Value("${local.server.port}")
+    int port;
+
+    private String       registrationApiUrl;
+    private RestTemplate restTemplate = new TestRestTemplate();
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    /**
+     * This is run before every @Test method
+     *
+     * NOTE: @Transactional and @Rollback or @TransactionConfiguration(defaultRollback = true) will NOT work here; You
+     * cannot create transaction based tests when making requests to the REST API, despite that you could autowire the
+     * transaction manager in your test. The reason is that any transaction defined in your application, regardless of
+     * where it is declared, will be committed before the server response is sent. Consequently, it will be too late for
+     * a test to roll back any state using the @Transactional annotation when the test completes.
+     */
+    @Before
+    public void setUp() {
+        registrationApiUrl = "http://localhost:" + port + "/register/";
+
+        // reset DB since transactions are not working
+        resetDb();
+    }
+
+    @Test
+    public void testRegisterUser_whenUserRegistrationIsSuccess_thenUserIsRegistered() throws Exception {
+        // prepare
+        long userCount = userRepository.count();
+        HttpEntity<RegistrationDto> registrationRequest = createRegistrationRequest();
+
+        // act
+        ResponseEntity<Void> responseEntity = restTemplate.postForEntity(registrationApiUrl, registrationRequest, Void.class);
+
+        // assert
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(userRepository.findAll()).isNotEmpty().hasSize((int) userCount + 1);
+    }
+
+    @Test
+    public void testRegisterUser_whenUserAlreadyExists_thenBadRequestIsReturned() throws Exception {
+        // prepare
+        assertThat(userRepository.findByEmail("CAFE@BABE.COM")).isNull();
+        HttpEntity<RegistrationDto> registrationRequest = createRegistrationRequest();
+
+        // act
+        ResponseEntity<Void> firstResponseEntity = restTemplate.postForEntity(registrationApiUrl, registrationRequest, Void.class);
+        ResponseEntity<Void> secondResponseEntity = restTemplate.postForEntity(registrationApiUrl, registrationRequest, Void.class);
+
+        // assert
+        assertThat(firstResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(secondResponseEntity.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void testRegisterUser_whenRegistrationIsMissingRequiredData_thenBadRequestIsReturned() throws Exception {
+        // prepare
+        HttpEntity<RegistrationDto> registrationRequest = createRegistrationRequest();
+        registrationRequest.getBody().setEmail(null);
+
+        // act
+        ResponseEntity<Void> responseEntity = restTemplate.postForEntity("http://localhost:" + port + "/register/", registrationRequest, Void.class);
+
+        // assert
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    // +------------ HELPER METHODS ------------+
+    private HttpEntity<RegistrationDto> createRegistrationRequest() {
+        RegistrationDto registrationDto = createRegistrationDto();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<RegistrationDto> request = new HttpEntity<RegistrationDto>(registrationDto, headers);
+        return request;
+    }
+
+    private RegistrationDto createRegistrationDto() {
+        RegistrationDto registrationDto = new RegistrationDto();
+        registrationDto.setEmail("CAFE@BABE.COM");
+        registrationDto.setPassword("SuperSecurePassword");
+        registrationDto.setPhoneNumber("666 999");
+        return registrationDto;
+    }
+
+    private void resetDb() {
+        ticketRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+}
