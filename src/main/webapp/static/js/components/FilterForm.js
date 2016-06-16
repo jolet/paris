133eb5af8256dfb28dcd9ac2627397a783ecacb4
denix00 <denix00@gmail.com>
2016-06-16 11:56:48 +0200

    filterForma - dio logike

--- src/main/webapp/static/js/components/FilterForm.js
+++ src/main/webapp/static/js/components/FilterForm.js
@@ -1,27 +1,47 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 
 export default class extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      type: '',
+      date: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  handleChange(name, event){
+    this.setState({[event.target.name]: event.target.value});
+    console.log({[event.target.name]: event.target.value});
+  }
 
   submit(event){
     event.preventDefault();
     console.log("Forma poslana");
+    console.log(this.state);
   }
+
 
   render() {
 
     return (
-      <form method="GET" action="events" onSubmit={this.submit}>
+      <form onSubmit={this.submit.bind(this)}>
          <h4><strong>Filtriraj</strong></h4>
          <div className="form-group row">
            <label className="col-sm-2 form-control-label" htmlFor="name">Naziv događaja:</label>
            <div className="col-sm-4">
-             <input type="text" name="name" id="name" className="form-control" />
+             <input type="text" name="name" id="name" className="form-control" value={this.state.name} onChange={this.handleChange.bind(this, 'name')}/>
            </div>
          </div>
          <div className="form-group row">
            <label className="col-sm-2 form-control-label" htmlFor="category">Vrsta događaja:</label>
            <div className="col-sm-4">
-           <select className="form-control">
+           <select className="form-control" onChange={this.handleChange.bind(this, 'type')}>
              <option defaultValue>Odaberi</option>
              <option value="prvi">Prvi</option>
              <option value="drugi">Drugi</option>
@@ -31,7 +51,7 @@ export default class extends React.Component {
          <div className="form-group row">
            <label className="col-sm-2 form-control-label" htmlFor="date">Datum:</label>
            <div className="col-sm-4">
-             <input type="date" name="date" id="date" className="form-control" />
+             <input type="date" name="date" id="date" className="form-control" onChange={this.handleChange.bind(this, 'date')}/>
            </div>
          </div>
          <input type="submit" className="btn btn-primary" defaultValue="Filtriraj" />
