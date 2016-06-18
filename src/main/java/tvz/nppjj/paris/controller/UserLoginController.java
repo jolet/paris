14a08b403812acc3c0756a1ef4c67e6e42e0c833
denix00 <denix00@gmail.com>
2016-06-19 00:03:03 +0200

    UserLoginController CrossOrigin

--- src/main/java/tvz/nppjj/paris/controller/UserLoginController.java
+++ src/main/java/tvz/nppjj/paris/controller/UserLoginController.java
@@ -3,6 +3,7 @@ package tvz.nppjj.paris.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ public class UserLoginController {
 	@Autowired
 	private UserService userService;
 	
+	@CrossOrigin
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public UserDto mapFields(@Valid @RequestBody UserLoginCommand userLoginCommand) {
 		return userService.loginUser(userLoginCommand.getUsername(), userLoginCommand.getPassword());
