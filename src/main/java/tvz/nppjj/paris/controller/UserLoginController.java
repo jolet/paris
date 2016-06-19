c7962ca56c5a7630e8bd5384e5e0af9c09163385
jkovacek <josipkovacek@yahoo.com>
2016-06-19 02:49:40 -0700

    added JWT auth

--- src/main/java/tvz/nppjj/paris/controller/UserLoginController.java
+++ src/main/java/tvz/nppjj/paris/controller/UserLoginController.java
@@ -9,21 +9,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import tvz.nppjj.paris.command.UserLoginCommand;
+import tvz.nppjj.paris.init.config.filter.JwtSignature;
+import tvz.nppjj.paris.model.dto.LoginResponseDto;
 import tvz.nppjj.paris.model.dto.UserDto;
 import tvz.nppjj.paris.service.UserService;
 
 @RestController
 public class UserLoginController {
 
-	@Autowired
-	private UserService userService;
-	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public UserDto mapFields(@Valid @RequestBody UserLoginCommand userLoginCommand) {
-		return userService.loginUser(userLoginCommand.getUsername(), userLoginCommand.getPassword());
-		
-		
-	}
-	
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public LoginResponseDto authenticateUser(@Valid @RequestBody UserLoginCommand userLoginCommand) {
+        UserDto userDto = userService.loginUser(userLoginCommand.getUsername(), userLoginCommand.getPassword());
+
+        LoginResponseDto loginResponseDto = new LoginResponseDto();
+        loginResponseDto.setUserDto(userDto);
+        loginResponseDto.setToken(JwtSignature.createJwtToken(userDto));
+        return loginResponseDto;
+
+    }
+
+    // TODO: remove after jwt integration
+    @RequestMapping(value = "/jwt", method = RequestMethod.POST)
+    public String jwtTest() {
+        return "jwt request OK";
+    }
 
 }
---
+++ src/main/java/tvz/nppjj/paris/init/config/filter/JwtFilter.java
@@ -0,0 +1,41 @@
+package tvz.nppjj.paris.init.config.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.UnsupportedJwtException;
+import tvz.nppjj.paris.model.exception.ParisException;
+
+public class JwtFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) req;
+
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new ParisException("Missing or invalid Authorization header - Nemas to pravo!");
+        }
+
+        final String token = authHeader.substring(7); // The part after "Bearer "
+
+        try {
+            final Claims claims = Jwts.parser().setSigningKey(JwtSignature.KEY.getValue()).parseClaimsJws(token).getBody();
+            request.setAttribute("claims", claims);
+        } catch (final UnsupportedJwtException e) {
+            throw new ServletException("Invalid token.");
+        }
+
+        chain.doFilter(req, res);
+    }
+
+}
