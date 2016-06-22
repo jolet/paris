d02df98bfcd033ec5f0204184385017eee750efb
ejosip93 <ejosip93@gmail.com>
2016-06-22 17:10:55 +0200

    dizajn user profila dodan

--- src/main/webapp/static/js/pages/UserProfile.js
+++ src/main/webapp/static/js/pages/UserProfile.js
@@ -1,16 +1,67 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { IndexLink, Link, Router, Route, hashHistory } from "react-router";
 
-export default class extends React.Component {
+export default class UserProfile extends React.Component {
 
-  constructor(props){
-    super(props);
-  }
+	constructor(props){
+	    super(props);
+	    this.state = {response:[]};
+	  }
 
+	  componentDidMount(){
+		    $.ajax({
+		      url: 'http://localhost:8080/users/{id}',
+		      context: this,
+		      dataType: 'json',
+		      type: 'GET'
+		    }).done(function (data){
+		      this.setState({response: data});
+		      // console.log(JSON.stringify(this.state.response));
+		    });
+		  }
   render() {
 
+	  
     return (
-      <p>userprofile</p>
+    		<div class="panel panel-info">
+    		<div class="panel-heading">Josip Perić</div>
+    		<div className="row">
+		     <div className="form-group col-sm-5">
+		
+		     </div>
+		   </div>
+    		  <div className="row">
+    		     <div className="form-group col-sm-5">
+    		     <b>&nbsp;  E-mail: </b>ejosip93@gmail.com
+    		     </div>
+    		   </div>
+    		   <div className="row">
+  		     <div className="form-group col-sm-5">
+  		
+  		     </div>
+  		   </div>
+      		  <div className="row">
+      		     <div className="form-group col-sm-5">
+      		   <b>&nbsp;  Phone number: </b>099/9999999
+      		     </div>
+      		   </div>
+      		 <div className="row">
+		     <div className="form-group col-sm-5">
+		
+		     </div>
+		   </div>
+    		  <div className="row">
+    		     <div className="form-group col-sm-5">
+    		     <b>&nbsp;  Account: </b>500.00
+    		     </div>
+    		   </div>
+    		
+    	      
+    	     
+
+      </div>
+    
     );
   }
 }
