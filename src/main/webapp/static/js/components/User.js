1f8e9a601929af81b2733271f4edfc115375aaa5
denix00 <denix00@gmail.com>
2016-06-30 11:19:28 +0200

    administracija korisnicima - jos dodati linkove na uredivanje i brisanje korisnika

---
+++ src/main/webapp/static/js/components/User.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { IndexLink, Link, Router, Route, hashHistory } from "react-router";
+
+export default class User extends React.Component {
+  render() {
+
+    const {user} = this.props;
+
+    return (
+      <tr>
+        <td>{user.id}</td>
+        <td>{user.username}</td>
+        <td>{user.email}</td>
+        <td>{user.role}</td>
+        <td>{user.phoneNumber}</td>
+        <td><Link to="#" class="btn btn-warning">Uredi</Link></td>
+        <td><Link to="#" class="btn btn-danger">Obriši</Link></td>
+      </tr>
+    );
+  }
+}
--- src/main/webapp/static/js/pages/UserList.js
+++ src/main/webapp/static/js/pages/UserList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import User from '../components/User';
+
 export default class UserList extends React.Component {
 
   constructor(props){
@@ -8,13 +10,35 @@ export default class UserList extends React.Component {
     }
 
   componentDidMount(){
+    $.ajax({
+      url: 'http://localhost:8080/users',
+      context: this,
+      dataType: 'json',
+      type: 'GET'
+    }).done(function (data){
+      this.setState({response: data});
+      console.log(JSON.stringify(this.state.response));
+    });
   }
 
   render() {
     return (
-      <div>
-        lista usera
-      </div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>Korisničko ime</td>
+            <td>Email</td>
+            <td>Rola</td>
+            <td>Tel. broj</td>
+            <td>Uredi</td>
+            <td>Obriši</td>
+          </tr>
+        </thead>
+        <tbody>
+          { this.state.response.map((usersAPI, i) => <User key={i} user={usersAPI} />)  }
+        </tbody>
+      </table>
     );
   }
 }
