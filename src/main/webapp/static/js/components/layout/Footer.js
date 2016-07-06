e420a1983287f042c39d23dd4f55b49d7e723cf4
denix00 <denix00@gmail.com>
2016-07-06 15:14:23 +0200

    build version u footeru

--- src/main/webapp/static/js/components/layout/Footer.js
+++ src/main/webapp/static/js/components/layout/Footer.js
@@ -1,17 +1,41 @@
 import React from "react";
+import { IndexLink, Link, Router, Route, hashHistory } from "react-router";
 
 
 export default class Footer extends React.Component {
-  render() {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            version: ''
+        };
+    }
+
+    componentDidMount(){
+        this.setState({
+            version: '1.1'
+        });
+
+        // $.ajax({
+        //     url: 'http://localhost:8080/changelog',
+        //     context: this,
+        //     dataType: 'json',
+        //     type: 'GET'
+        // }).done(function (data){
+        //     this.setState({version: data});
+        // });
+    }
+
+    render() {
     return (
       <footer>
         <div class="row">
           <div class="col-lg-12">
             <hr />
-            <p>Tim Paris 2016</p>
+            <p>Tim Paris 2016 | <span className="build-version"> version: {this.state.version} </span></p>
           </div>
         </div>
       </footer>
     );
-  }
+    }
 }
