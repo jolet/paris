64868c663525e8ba60e0871b429c244a69b615f7
mpezo <mpezo@tvz.hr>
2016-07-07 08:27:46 +0200

    eventController - problemi oko datuma

--- src/main/java/tvz/nppjj/paris/controller/EventController.java
+++ src/main/java/tvz/nppjj/paris/controller/EventController.java
@@ -1,16 +1,24 @@
 package tvz.nppjj.paris.controller;
 
+
+
+
 import java.sql.Date;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import tvz.nppjj.paris.model.Category;
 import tvz.nppjj.paris.model.dto.EventCommand;
 import tvz.nppjj.paris.model.dto.EventDto;
 import tvz.nppjj.paris.model.dto.PaginationDto;
@@ -18,6 +26,9 @@ import tvz.nppjj.paris.service.EventService;
 
 @RestController
 public class EventController {
+    
+    @SuppressWarnings("deprecation")
+    private Date dateCompare = new Date(1970, 01, 01); 
 
     @Autowired
     private EventService eventService;
@@ -29,18 +40,20 @@ public class EventController {
     }
 
     @CrossOrigin
-    @RequestMapping(value = "/eventsFilter", params = { "name", "categoryId", "date" }, method = RequestMethod.GET)
-    public @ResponseBody List<EventDto> getFilteredEvents(@RequestParam(required = false, value = "name") String name,
-            @RequestParam(required = false, value = "categoryId") Long categoryId,
-            @RequestParam(required = false, value = "date") Date date) {
+    @RequestMapping(value = "/eventsFilter", method = RequestMethod.GET)
+    public @ResponseBody List<EventDto> getFilteredEvents(@RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "categoryId", required = false) Long categoryId,
+            @RequestParam(value = "date", required = false, defaultValue="1970-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
 
-        if(name==null && categoryId==null && date == null){
-            return eventService.getAllEvents();
-        }
-        else{
-            return eventService.getFilteredEvents(name, categoryId, date);  
-        }
         
+        
+        
+        if (name == null && categoryId == null && date.compareTo(dateCompare)==0) {
+            return eventService.getAllEvents();
+        } else {
+            return eventService.getFilteredEvents(name, categoryId, date);
+        }
+
     }
 
     @CrossOrigin
