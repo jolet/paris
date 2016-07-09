2de833d7662b7b40a2c5c990235ed701229279d5
mpezo <mpezo@tvz.hr>
2016-07-09 22:00:00 +0200

    TicketType Repository

---
+++ src/main/java/tvz/nppjj/paris/repository/TicketTypeRepository.java
@@ -0,0 +1,9 @@
+package tvz.nppjj.paris.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tvz.nppjj.paris.model.TicketType;
+
+public interface TicketTypeRepository extends JpaRepository<TicketType, Long> {
+
+}
