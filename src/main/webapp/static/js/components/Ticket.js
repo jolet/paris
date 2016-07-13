6755d2aa74eec67e0a72c399428cb61c7568a73c
ejosip93 <ejosip93@gmail.com>
2016-07-13 02:57:20 +0200

    Dodan link na event sa ticketa

--- src/main/webapp/static/js/components/Ticket.js
+++ src/main/webapp/static/js/components/Ticket.js
@@ -38,7 +38,8 @@ export default class Ticket extends React.Component {
 	     <img className="homepage-imgs" src="http://crooksandliars.com/files/primary_image/15/05/carlos-santana.jpg"/>
 	     </div> 
 	     <div className="form-group col-sm-4">
-	     <h2>{ticket.event.name}</h2>
+         <Link to={"events/" + ticket.event.id} ><h2>{ticket.event.name}</h2></Link> 
+	     
 	     <b>Lokacija: </b>{ticket.event.city}, {ticket.event.location}<br/>
 	     <b>Cijena: </b>{ticket.price}<br/>
 	     <b>{ticket.event.date}</b>
