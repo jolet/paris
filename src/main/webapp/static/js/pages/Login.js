742f25ac97eb8b9ad748bfc5bf97a907e8ae7faf
denix00 <denix00@gmail.com>
2016-07-13 02:23:15 +0200

    dodan link za registraciju na login

--- src/main/webapp/static/js/pages/Login.js
+++ src/main/webapp/static/js/pages/Login.js
@@ -1,119 +1,121 @@
 import React from "react";
+import { IndexLink, Link, Router, Route, hashHistory } from "react-router";
 import AlertDanger from '../components/AlertDanger';
 
 export default class Login extends React.Component {
 
-      constructor(props){
-            super(props);
-            this.state = {
-              username: '',
-              password: '',
-                error:[]
-            }
-            this.handleChange = this.handleChange.bind(this);
-            this.submit = this.submit.bind(this);
-          }
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            error:[]
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.submit = this.submit.bind(this);
+    }
 
-      submit(event){
-          var ovaKlasa = this;
+    submit(event){
+        var ovaKlasa = this;
 
-            event.preventDefault();
-            console.log("Prijava ce se izvrsiti");
-            console.log(this.state);
+        event.preventDefault();
+        console.log("Prijava ce se izvrsiti");
+        console.log(this.state);
 
-            var headers = new Headers({"Content-type": "application/json"});
-            var body = JSON.stringify({
-              username: this.state.username,
-              password: this.state.password,
-            });
+        var headers = new Headers({"Content-type": "application/json"});
+        var body = JSON.stringify({
+            username: this.state.username,
+            password: this.state.password,
+        });
 
-            var init = {
-              method: 'POST',
-              headers: headers,
-              body: body,
-              mode: 'cors',
-            }
+        var init = {
+            method: 'POST',
+            headers: headers,
+            body: body,
+            mode: 'cors',
+        }
 
-            var url = new Request( localStorage.getItem("environmentPrefix") + '/login');
+        var url = new Request( localStorage.getItem("environmentPrefix") + '/login');
 
-            fetch(url, init)
+        fetch(url, init)
             .then((response) => response.json())
             .then((responseData) => {
-              if(responseData.status == 500){
-                  let temp = [];
-                  temp.push(responseData);
-                  ovaKlasa.setState({
-                      error: temp
-                  });
-              }else{
+                if(responseData.status == 500){
+                    let temp = [];
+                    temp.push(responseData);
+                    ovaKlasa.setState({
+                        error: temp
+                    });
+                }else{
 
-                  localStorage.setItem("registrationStatus", "");
-                localStorage.setItem("token", 'Bearer ' + responseData.token);
-                localStorage.setItem("username", responseData.userDto.username);
-                localStorage.setItem("email", responseData.userDto.email);
-                localStorage.setItem("id", responseData.userDto.id);
-                localStorage.setItem("role", responseData.userDto.role);
+                    localStorage.setItem("registrationStatus", "");
+                    localStorage.setItem("token", 'Bearer ' + responseData.token);
+                    localStorage.setItem("username", responseData.userDto.username);
+                    localStorage.setItem("email", responseData.userDto.email);
+                    localStorage.setItem("id", responseData.userDto.id);
+                    localStorage.setItem("role", responseData.userDto.role);
 
-                  //ako je preusmjeren s neke stranice, vrati ga tamo
-                  if(! localStorage.getItem("url") == ""){
-                      let temp = localStorage.getItem("url");
-                      localStorage.setItem("url", "");
-                      location.href = temp;
-                  }else{
-                      location.href = '/#/';
-                  }
-              }
+                    //ako je preusmjeren s neke stranice, vrati ga tamo
+                    if(! localStorage.getItem("url") == ""){
+                        let temp = localStorage.getItem("url");
+                        localStorage.setItem("url", "");
+                        location.href = temp;
+                    }else{
+                        location.href = '/#/';
+                    }
+                }
             });
-          }
+    }
 
 
-      handleChange(name, event){
+    handleChange(name, event){
         this.setState({[event.target.name]: event.target.value});
         console.log({[event.target.name]: event.target.value});
-      }
+    }
 
- render() {
-   return (
+    render() {
+        return (
 
-     <div>
+            <div>
 
-         {localStorage.getItem("registrationStatus") == "true"
-             ?
-         <div class="alert alert-success">
-             <strong>Registracija je prošla uspješno!</strong> Molimo, prijavite se.
-         </div>
-             : null}
+                {localStorage.getItem("registrationStatus") == "true"
+                    ?
+                    <div class="alert alert-success">
+                        <strong>Registracija je prošla uspješno!</strong> Molimo, prijavite se.
+                    </div>
+                    : null}
 
-         {! localStorage.getItem("url") == ""
-             ?
-             <div class="alert alert-warning">
-                 <strong>Prijavi se kako bi kupnja bila moguća!</strong>
-             </div>
-             : null}
+                {! localStorage.getItem("url") == ""
+                    ?
+                    <div class="alert alert-warning">
+                        <strong>Prijavi se kako bi kupnja bila moguća!</strong>
+                    </div>
+                    : null}
 
 
 
-         { this.state.error.map((error, i) => <AlertDanger key={i} error={error.message} />)  }
+                { this.state.error.map((error, i) => <AlertDanger key={i} error={error.message} />)  }
 
-     <h2><strong>Prijava</strong></h2>
-     <form onSubmit={this.submit}>
-     <div className="row">
-     <div className="form-group col-sm-5">
-     Korisničko ime:
-     <input type="text" pattern="^(?=.{4,100}$).*$" title="Korisničko ime sadržava 4-100 znakova" name="username" className="form-control" required onChange={this.handleChange.bind(this, 'username')} />
-     </div>
-     </div>
-     <div className="row">
-     <div className="form-group col-sm-5">
-     Lozinka:
-     <input type="password" pattern="^(?=.{6,16}$).*$" title="Lozinka sadržava 8-16 znakova" name="password" id="pass1" className="form-control" required onChange={this.handleChange.bind(this, 'password')} />
-     </div>
-     </div>
-     <div className="row">
-     <input type="submit" id="btnUserLogin" defaultValue="Prijava" className="btn btn-primary pull-right" />
-     </div>
-     </form>
-     </div>
-   );
- }
+                <h2><strong>Prijava</strong></h2>
+                <form onSubmit={this.submit}>
+                    <div className="row">
+                        <div className="form-group col-sm-5">
+                            Korisničko ime:
+                            <input type="text" pattern="^(?=.{4,100}$).*$" title="Korisničko ime sadržava 4-100 znakova" name="username" className="form-control" required onChange={this.handleChange.bind(this, 'username')} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-sm-5">
+                            Lozinka:
+                            <input type="password" pattern="^(?=.{6,16}$).*$" title="Lozinka sadržava 8-16 znakova" name="password" id="pass1" className="form-control" required onChange={this.handleChange.bind(this, 'password')} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <input type="submit" id="btnUserLogin" defaultValue="Prijava" className="btn btn-primary pull-right" />
+                    </div>
+                </form>
+                Niste registrirani? <Link to="/registration" >Registracija</Link>
+            </div>
+        );
+    }
 }
